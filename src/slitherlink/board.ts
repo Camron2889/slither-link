d4fcb2179990a62b5d2801aa9dd5738888739a74
Camron2889 <Camron2889@gmail.com>
2025-07-20 02:30:57 -0400

    Update board.ts

--- src/slitherlink/board.ts
+++ src/slitherlink/board.ts
@@ -3,12 +3,39 @@ export interface SlitherlinkBoardConfig {
   height: number;
 }
 
+export interface CellConfig {
+  x: number;
+  y: number;
+}
+
+export class Cell {
+  constructor() {
+
+  }
+}
+
 export class SlitherlinkBoard {
   readonly width: number;
   readonly height: number;
+  readonly matrixWidth: number;
+  readonly matrixHeight: number;
+
+  private matrix: number[][];
 
   constructor(config: SlitherlinkBoardConfig) {
-    this.width = config.width;
+    if (config.width === undefined || config.height === undefined ) {
+      throw new Error("Board dimensions must be passed in config object.");
+    } else if (config.width <= 0 || config.height <= 0) {
+      throw new Error("Board dimensions must be greater than 0.");
+    }
+    this.width = config.width; 
     this.height = config.height;
+    this.matrixWidth = config.width * 2 + 1;
+    this.matrixHeight = config.height * 2 + 1;
+
+    this.matrix = [];
+    for (let row = 0; row < this.matrixHeight; row++) {
+      this.matrix[row] = Array(this.matrixWidth).fill(-1);
+    }
   }
 }
